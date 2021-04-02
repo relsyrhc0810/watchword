e28b5b5ec6a3b5b12063b2ce5ca511066ea84111
Chrysler Carreon <relsyrhc_0810@yahoo.com>
2021-04-02 14:18:28 -0500

    Created var of arrays, character randomizer, var to store the selected parameter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,139 @@
 // Assignment code here
+//assigned the variable inputs
+
+//Array of Lower Case characters
+var lowerCaseChar = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z'
+];
+
+//Array of Upper Case characters
+var upperCaseChar = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z'
+];
+
+//Array of numbers
+var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//Array of symbols
+var symbolChar = [
+	'@',
+	'%',
+	'+',
+	'\\',
+	'/',
+	"'",
+	'!',
+	'#',
+	'$',
+	'^',
+	'?',
+	':',
+	',',
+	')',
+	'(',
+	'}',
+	'{',
+	']',
+	'[',
+	'~',
+	'-',
+	'_',
+	'.'
+];
 
 // getting your password parameters
-
 function passwordParameters() {
-	var numOfChar = prompt("How many Character you want? Min of 6 & Max of 20 Characters")
-	var numOfChar = parseInt(numOfChar)
-	// console.log(typeof numOfChar, numOfChar)
+	var numOfChar = parseInt(prompt("How many Character you want? Min of 6 & Max of 20 Characters"))
 
 	// minimum & maximum length of password 
-	if (numOfChar < 5|| numOfChar > 20 || isNaN(numOfChar) === true) {
+	if (numOfChar < 6 || numOfChar > 20 || isNaN(numOfChar) === true) {
 		alert("Enter valid value - Min of 6 & Max of 20 Characters")
 	};
 
-	var lowerCase = confirm("Do like to have lower case?")
-	var upperCase = confirm("Do like to have upper case?")
-	var symbol = confirm("Do like to have symbol")
+	// select character symbol & number
+	var lowerCase = confirm("Do like to have lower case character?")
+	var upperCase = confirm("Do like to have upper case character?")
+	var symbol = confirm("Do like to have symbol?")
+	var num = confirm("Do like to have number?")
 
 	// making sure atleast they select one of the parameters
-	if (lowerCase === false  || upperCase === false || symbol === false) {
-		alert("you have to choose one of the parameters")
+	if (lowerCase === false && upperCase === false && symbol === false && num === false) {
+		alert("You have to choose one of the parameters")
 	} else {
-		alert("Password minimum parameters requirements is OK ")
-	}s
+		alert("Password minimum parameter requirements is OK!")
+	}
+
+	//this is where we stored the inputed parameters
+	var enteredParameters = {
+		length: numOfChar,
+		lowerCase: lowerCase,
+		upperCase: upperCase,
+		symbol: symbol,
+		num: num
+	}
+	return enteredParameters
+
 };
 
-// passwordParameters();
-function getLowerCase () {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+//function that will select 
+function randomizer(array) {
+	var random = Math.floor(Math.random() * array.length)
+	var randomIndex = array[random]
+
+	return randomIndex
 }
-function getUpperCase () {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function getNumber () {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getSymbol () {
-	return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-}
-console.log(getLowerCase());
+
+
 
 
 // Get references to the #generate element
@@ -45,10 +141,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
